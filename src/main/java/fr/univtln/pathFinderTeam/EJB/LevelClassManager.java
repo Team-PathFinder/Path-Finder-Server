6b8896367eb589de144c372e65e3e82a199649ca
clement farge <farge.clement@gmail.com>
2018-01-19 11:51:58 +0100

    [gitflow]squashing 'feature-add-request-for-levelClass' into 'development'

--- src/main/java/fr/univtln/pathFinderTeam/EJB/LevelClassManager.java
+++ src/main/java/fr/univtln/pathFinderTeam/EJB/LevelClassManager.java
@@ -1,7 +1,9 @@
 package fr.univtln.pathFinderTeam.EJB;
 
 import fr.univtln.pathFinderTeam.DAO.CrudService;
+import fr.univtln.pathFinderTeam.DAO.QueryParameter;
 import fr.univtln.pathFinderTeam.classes.LevelClass;
+import fr.univtln.pathFinderTeam.classes.RPCharacter;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
@@ -12,6 +14,11 @@ public class LevelClassManager implements Serializable {
 
     @EJB
     CrudService<LevelClass> cs;
+
+    public LevelClass findByName(String pName) {
+
+        return (LevelClass) cs.findWithNamedQuery("findByName", QueryParameter.with("pName",pName).parameters()).get(0);
+    }
 
     public LevelClassManager(){}
 
